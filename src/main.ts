0090cd1705972833f7460de113ea2f956407f998
Mike <misha.o.son@gmail.com>
2024-07-28 21:13:24 +0300

    jwt auth

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser'
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api')
+  app.use(cookieParser())
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+    exposedHeaders: "set-cookie"
+  })
+  
+  await app.listen(process.env.SERVER_PORT);
+}
+bootstrap();
